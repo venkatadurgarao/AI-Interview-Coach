42d9ca0e229bd7fb9aece8c42ba258f8d60bcd2f
508 Bytes <kishorebabu.mc@gmail.com>
2026-06-20 11:24:33 +0530

    changed react router mode to data

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,7 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    tsconfigPaths: true,
-  },
-});
+  plugins: [react()],
+})
